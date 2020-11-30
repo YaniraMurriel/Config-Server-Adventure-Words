829e4b0d8e3b3aedc20642458aa7f281ea666d04
YaniraMurriel <yaniscase@hotmail.com>
2020-11-30 18:21:27 -0500

    Prueba properties

--- src/main/java/com/example/adventureworks/configserver/ConfigserverApplication.java
+++ src/main/java/com/example/adventureworks/configserver/ConfigserverApplication.java
@@ -2,8 +2,11 @@ package com.example.adventureworks.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
+
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
